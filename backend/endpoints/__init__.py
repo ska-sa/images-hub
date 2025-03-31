0869d631f73294d3578e1209f41c4e443a5346e2
shlabisa <shlabisa@sarao.ac.za>
2025-03-31 12:07:44 +0200

    Added an importation of functions in utils.py

--- backend/endpoints/__init__.py
+++ backend/endpoints/__init__.py
@@ -3,3 +3,4 @@ from .users import get_users, get_user, auth, google_auth
 from .images import get_images, get_image, get_image_s3_url, upload_image, put_image, delete_image
 from .requests import get_requests, get_request, post_request, put_request, delete_request
 from .links import get_links, get_link, download_image, post_link, put_link, delete_link
+from .utils import apply_randomness, get_recommended_images
